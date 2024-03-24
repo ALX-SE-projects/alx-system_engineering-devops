7e39bb42a7e85780a7a9f75d63880f9f8b72618f
ibrahemesam <iimosa7777@gmail.com>
2024-03-25 00:27:37 +0200

    blabla bla

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Install flask
 exec { 'install_flask':
- command => 'pip3 install Flask==2.1.0',
+ command => "pip3 install 'Flask==2.1.0'",
  path    => ['/usr/local/bin', '/usr/bin'],
- unless => 'pip3 show Flask | grep "Version: 2.1.0"',
+ unless => "pip3 show Flask | grep 'Version: 2.1.0'",
 }
