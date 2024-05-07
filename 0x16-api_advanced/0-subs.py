77a63211b41ad89dcb1caba2f2bc3a24db9d105c
ibrahemesam <iimosa7777@gmail.com>
2024-05-08 02:21:44 +0300

    blabla bla

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,15 @@
+"How many subs?"
 from requests import get as GET
 
 
 def number_of_subscribers(subreddit):
+    "subreddit: name of subreddit"
     r = GET(
         f"http://reddit.com/r/{subreddit}/about.json",
         headers={'User-Agent': "alx"},
         # allow_redirects=False
         ).json()['data']
-    # r = r.json()
-    # print(r)
     if 'subscribers' in r:
         return r['subscribers']
     else:
         return 0
-    # print(r.text)
